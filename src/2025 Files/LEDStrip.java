6ffe9187b51eafadc4bbee6076b93213ed89a67c
Odysseus <101147725+3dWonderGuy@users.noreply.github.com>
2026-01-13 21:09:46 -0600

    no code changes. just imported some more files from 2025 that might be reused

---
+++ src/2025 Files/LEDStrip.java
@@ -0,0 +1,124 @@
+// Copyright (c) 2025 FRC 2290
+// http://https://github.com/frc2290
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
+package frc.utils;
+
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.AddressableLEDBufferView;
+import edu.wpi.first.wpilibj.util.Color;
+import frc.utils.LEDEffects.LEDEffect;
+
+/** Represents a named subset of the full LED buffer that can be animated independently. */
+public class LEDStrip {
+  /** Descriptive name so the coordinator can reference the strip. */
+  String name;
+
+  /** Starting index in the master LED buffer. */
+  int start;
+
+  /** Ending index (exclusive) in the master LED buffer. */
+  int stop;
+
+  /** View into the master buffer used to render effects. */
+  AddressableLEDBufferView bufferView;
+
+  /** Base color for effects when no override is supplied. */
+  Color color = LEDEffects.flytBlue;
+
+  /** Active animation effect to apply. */
+  LEDEffect effect = LEDEffect.SOLID;
+
+  /** Generic helper flag that allows custom behaviors to annotate strips. */
+  boolean helperBool = false;
+
+  public LEDStrip(String _name, AddressableLEDBuffer _buffer, int _start, int _stop) {
+    name = _name;
+    start = _start;
+    stop = _stop;
+    bufferView = _buffer.createView(start, stop);
+  }
+
+  public LEDStrip(
+      String _name, AddressableLEDBuffer _buffer, int _start, int _stop, boolean _reversed) {
+    name = _name;
+    start = _start;
+    stop = _stop;
+    bufferView = _buffer.createView(start, stop);
+    bufferView = bufferView.reversed();
+  }
+
+  public AddressableLEDBufferView getBufferView() {
+    return bufferView;
+  }
+
+  public void setBufferView(AddressableLEDBuffer _buffer) {
+    if (bufferView.isReversed()) {
+      bufferView = _buffer.createView(start, stop);
+      bufferView = bufferView.reversed();
+    } else {
+      bufferView = _buffer.createView(start, stop);
+    }
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public LEDEffect getEffect() {
+    return effect;
+  }
+
+  public boolean getReversed() {
+    return bufferView.isReversed();
+  }
+
+  public Color getColor() {
+    return color;
+  }
+
+  public int getStart() {
+    return start;
+  }
+
+  public int getStop() {
+    return stop;
+  }
+
+  public int getLength() {
+    return bufferView.getLength();
+  }
+
+  public void setEffect(LEDEffect _effect) {
+    effect = _effect;
+  }
+
+  public void setEffect(LEDEffect _effect, Color _color) {
+    effect = _effect;
+    color = _color;
+  }
+
+  public void setColor(Color _color) {
+    color = colorUtils.gammaCorrection(_color);
+  }
+
+  public boolean getHelperBool() {
+    return helperBool;
+  }
+
+  public void setHelperBool(boolean helper) {
+    helperBool = helper;
+  }
+}
---
+++ src/2025 Files/Robot.java
@@ -0,0 +1,211 @@
+// Copyright (c) 2025 FRC 2290
+// http://https://github.com/frc2290
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
+package frc.robot;
+
+import au.grapplerobotics.CanBridge;
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.ClimbSubsystem;
+import frc.robot.subsystems.DifferentialSubsystem;
+import frc.robot.subsystems.DriveStateMachine;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.ElevatorSubsystem;
+import frc.robot.subsystems.ManipulatorStateMachine;
+import frc.robot.subsystems.ManipulatorSubsystem;
+import frc.robot.subsystems.StateMachineCoordinator;
+import frc.robot.subsystems.StateMachineCoordinator.RobotState;
+import frc.utils.LEDUtility;
+import frc.utils.PoseEstimatorSubsystem;
+import org.littletonrobotics.urcl.URCL;
+
+/**
+ * Primary program entry point for Excelsior. {@link TimedRobot} periodically calls each lifecycle
+ * hook so we can schedule commands, monitor subsystems, and react to driver inputs. The goal of
+ * this class is to orchestrate subsystem construction and defer logic to the command-based
+ * framework.
+ */
+public class Robot extends TimedRobot {
+  /** Cached handle to the active autonomous routine so we can cancel or reschedule if needed. */
+  private Command m_autonomousCommand;
+
+  /** Container that wires up all subsystems, commands, and driver controls. */
+  private RobotContainer m_robotContainer;
+
+  /** Dedicated LED helper that allows the state machine to communicate robot status. */
+  private final LEDUtility m_ledUtility = new LEDUtility(0);
+
+  /** Cached reference to the primary driver controller so subsystems can read axes. */
+  private XboxController m_driver = new XboxController(0);
+
+  // The robot's subsystems.
+  /** Owns all hardware for swerve driving and exposes the drive commands. */
+  private final DriveSubsystem m_robotDrive = new DriveSubsystem();
+
+  /** Combines vision and gyro data to maintain the best guess at the robot's pose. */
+  private final PoseEstimatorSubsystem m_poseEstimator = new PoseEstimatorSubsystem(m_robotDrive);
+
+  /** Elevator carriage that moves the manipulator up and down the reef. */
+  private final ElevatorSubsystem m_elevator = new ElevatorSubsystem();
+
+  /** Roller intake and sensors that interact directly with game pieces. */
+  private final ManipulatorSubsystem m_manipulator = new ManipulatorSubsystem();
+
+  /** Differential arm that provides extension and rotation for the manipulator. */
+  private final DifferentialSubsystem m_DiffArm = new DifferentialSubsystem();
+
+  /** Hooking subsystem used during endgame climbs. */
+  private final ClimbSubsystem m_climber = new ClimbSubsystem();
+
+  /** Coordinates all autonomous and teleop driving modes. */
+  private final DriveStateMachine m_driveStateMachine =
+      new DriveStateMachine(m_robotDrive, m_poseEstimator, m_driver);
+
+  /** Tracks and sequences manipulator states to guarantee safe transitions. */
+  private final ManipulatorStateMachine m_manipulatorStateMachine =
+      new ManipulatorStateMachine(m_DiffArm, m_elevator, m_manipulator, m_climber);
+
+  /** Central coordinator that keeps drive and manipulator state machines in sync. */
+  private final StateMachineCoordinator m_coordinator =
+      new StateMachineCoordinator(m_manipulatorStateMachine, m_driveStateMachine, m_ledUtility);
+
+  public Robot() {
+    // Ensure the CANivore bridge is connected before any CAN devices are created.
+    CanBridge.runTCP();
+  }
+
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  @Override
+  public void robotInit() {
+    // Instantiate the container so bindings, chooser registration, and default commands run once.
+    // All command bindings and default commands live in the container; create it once here.
+    m_robotContainer =
+        new RobotContainer(
+            m_ledUtility,
+            m_robotDrive,
+            m_poseEstimator,
+            m_elevator,
+            m_manipulator,
+            m_DiffArm,
+            m_climber,
+            m_driveStateMachine,
+            m_manipulatorStateMachine,
+            m_coordinator,
+            m_driver);
+    DataLogManager.start();
+    DriverStation.startDataLog(DataLogManager.getLog());
+    // Start the logging framework so we can view graphs after a match or practice run.
+    URCL.start();
+
+    // If logging only to DataLog.
+    URCL.start(DataLogManager.getLog());
+
+    // Display the Command Scheduler Status
+    SmartDashboard.putData(CommandScheduler.getInstance());
+
+    // Display Subsystem Status
+    SmartDashboard.putData(m_elevator);
+    SmartDashboard.putData(m_DiffArm);
+    SmartDashboard.putData(m_manipulator);
+    SmartDashboard.putData(m_robotDrive);
+  }
+
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+    // Run the scheduler to poll buttons, execute commands, and invoke subsystem periodic methods.
+    CommandScheduler.getInstance().run();
+  }
+
+  /** This function is called once each time the robot enters Disabled mode. */
+  @Override
+  public void disabledInit() {
+    // Notify subsystems that the robot is disabled so they can stop any motion and update LEDs.
+    m_coordinator.robotDisabled(true);
+  }
+
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+  @Override
+  public void autonomousInit() {
+    // Mark that the robot is in autonomous so the state machines can choose the correct defaults.
+    m_coordinator.robotAuto(true);
+    m_coordinator.robotDisabled(false);
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    /*
+     * String autoSelected = SmartDashboard.getString("Auto Selector",
+     * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
+     * = new MyAutoCommand(); break; case "Default Auto": default:
+     * autonomousCommand = new ExampleCommand(); break; }
+     */
+
+    // Schedule the autonomous command if one was selected.
+    if (m_autonomousCommand != null) {
+      // Defer command execution to the scheduler so it can manage interruptions properly.
+      m_autonomousCommand.schedule();
+    }
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {
+    m_coordinator.robotAuto(false);
+    m_coordinator.robotDisabled(false);
+    // Reset both state machines back to their safe starting configuration for teleop.
+    m_coordinator.setRobotGoal(RobotState.START_POSITION);
+    // Reset hook for post-autonomous tweaks if we ever need to retune teleop initialization.
+    // m_state.setCurrentElevManiStateCommand(ElevatorManipulatorState.SafeCoralTravel);
+
+    // Uncomment if we ever need to forcefully cancel the autonomous command during teleop init.
+    // if (m_autonomousCommand != null) {
+    //   m_autonomousCommand.cancel();
+    // }
+  }
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {}
+
+  @Override
+  public void testInit() {
+    // Cancels all running commands at the start of test mode.
+    CommandScheduler.getInstance().cancelAll();
+  }
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
+}
