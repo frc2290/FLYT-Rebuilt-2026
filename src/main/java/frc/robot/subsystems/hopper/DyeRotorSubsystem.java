82e67f51444e8559c36d2f6b911a474589c6facb
Odysseus <101147725+3dWonderGuy@users.noreply.github.com>
2026-02-07 11:13:33 -0600

    get dye stuff in

---
+++ src/main/java/frc/robot/subsystems/hopper/DyeRotorSubsystem.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems.hopper;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DyeRotorSubsystem extends SubsystemBase {
+    public DyeRotorSubsystem() {
+        
+    }
+
+    @Override
+    public void periodic() {
+        // This method is called every 20ms by the robot framework
+    }
+    
+}
