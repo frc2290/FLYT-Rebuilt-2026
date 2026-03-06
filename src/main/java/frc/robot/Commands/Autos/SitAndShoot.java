1a6e44c836a465e8632dde60eecf2384ed08974b
nacamm <nate@natecamm.dev>
2026-03-06 11:53:28 -0600

    latest and greatest

--- src/main/java/frc/robot/Commands/Autos/SitAndShoot.java
+++ src/main/java/frc/robot/Commands/Autos/SitAndShoot.java
@@ -5,6 +5,7 @@
 package frc.robot.Commands.Autos;
 
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.math.geometry.Pose2d;
 import frc.robot.subsystems.StateMachines.StateMachine;
 import frc.utils.PoseEstimatorSubsystem;
@@ -16,6 +17,8 @@ public class SitAndShoot extends FlytSequentialAuto {
     private StateMachine stateMachine;
     private PoseEstimatorSubsystem pose;
     private boolean right = true;
+    private Pose2d rightPose = new Pose2d(3.66, 0.72, Rotation2d.fromDegrees(90));
+    private Pose2d leftPose = new Pose2d(3.6, 7.28, Rotation2d.fromDegrees(-90));
 
     /** Creates a new SitAndShoot. */
     public SitAndShoot(StateMachine stateMachine, PoseEstimatorSubsystem pose) {
@@ -25,7 +28,8 @@ public class SitAndShoot extends FlytSequentialAuto {
 
     @Override
     public void setup() {
-        addCommands(pose.setCurrentPoseCommand((this.right ? new Pose2d(3.66, 0.72, Rotation2d.fromDegrees(0)) : new Pose2d(3.6, 7.28, Rotation2d.fromDegrees(180)))), 
+        addCommands(pose.setCurrentPoseCommand((this.right ? rightPose : leftPose)),
+                    Commands.runOnce(() -> pose.setTargetPose((this.right ? rightPose : leftPose))),
                     this.stateMachine.setShooterOverrideCommand(true));
     }
 
