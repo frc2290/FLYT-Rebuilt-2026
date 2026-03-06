355f1325c522496de9e26c68a7b1f4165fdff535
nacamm <nate@natecamm.dev>
2026-03-05 21:49:34 -0600

    Missed a cam

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -98,7 +98,7 @@ public final class Constants {
         // Multi-camera AprilTag rig configuration (robot-to-camera transforms).
         public static final String kForwardCamName = "forward";
         public static final Transform3d kForwardCamTransform = new Transform3d(
-                new Translation3d(0.24765, inchesToMeters(6), 0.5334),
+                new Translation3d(inchesToMeters(9.755440), inchesToMeters(6), inchesToMeters(20.420752)),
                 new Rotation3d(0.0, degreesToRadians(-42.0), 0.0));
 
         public static final String kBackwardCamName = "backward";
