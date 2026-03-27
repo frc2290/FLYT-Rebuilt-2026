65581e86bef2f620c8c6c90a439afbf63abd5bc5
nacamm <nate@natecamm.dev>
2026-03-27 09:26:07 -0500

    Update at 'Fri Mar 27 09:26:07 CDT 2026'

--- src/main/java/frc/robot/subsystems/turret/TurretIOSpark.java
+++ src/main/java/frc/robot/subsystems/turret/TurretIOSpark.java
@@ -83,8 +83,8 @@ public class TurretIOSpark implements TurretIO {
         // Setup RoboRio Absolute encoders
         // Initializes duty cycle encoders on DIO pins 0 and 1.
         // get() returns position in rotations [0, 1].
-        turnEncoder1 = new DutyCycleEncoder(8, 1, 0.1735929043398226); // Large
-        turnEncoder2 = new DutyCycleEncoder(9, 1, 0.5452482136312053); // Small
+        turnEncoder1 = new DutyCycleEncoder(8, 1, 0.0); // Large
+        turnEncoder2 = new DutyCycleEncoder(9, 1, 0.0); // Small
 
         // Setup Controllers
         turretController = turretSpark.getClosedLoopController();
